11aa089d1fb08d461ceecdf658939d0c893bc3f9
iamtylerd <tylerdanielphotography@gmail.com>
2016-06-02 14:47:00 -0500

    add json function

--- songs.js
+++ songs.js
@@ -15,10 +15,44 @@ for (var i = 0; i<songs.length; i++) {
 	currentSong = songs[i];
 	var cleanUp = currentSong.replace(/@|\(|\!|\*/g, '');
 	var dashSong = cleanUp.replace(/>/g, '-');
-	console.log(dashSong);
+	// console.log(dashSong);
 }
 
 
+// var songArray = document.getElementsByClassName("song-array");
+
+// for (var i = 0; i < songArray.length; i++) {
+// 	if ([i] % 2 === 0) {
+// 		songArray[i].classList.add("mintcream");
+// 	} else {
+// 		songArray[i].classList.add("seashell");
+// 	};
+// 	if ([i] <= 0) {
+// 		songArray[i].classList.add("boldtext");
+// 	};
+// }
+
+// XHR
+
+function songsLoaded () {
+  // A bit more about what `this` is here. What is the execution context?
+  var data = JSON.parse(this.responseText);
+  console.log(data);
+
+	var songDiv = document.getElementById("songId");
+	var thing = "";
+
+for (var i = 0; i < data.songs.length; i++) {
+	thing += `<div class='song${i} song-array'>`;
+	thing += `<h1>${data.songs[i].title}</h1>`;
+	thing += "<ul class='song-details'>";
+	thing += `<li>${data.songs[i].artist}</li>`;
+	thing += `<li>${data.songs[i].album}</li>`;
+	thing += `<li>${data.songs[i].genre}</li>`;
+	thing += "</ul>";
+	thing += "</div>";
+	songDiv.innerHTML = thing
+}
 var songArray = document.getElementsByClassName("song-array");
 
 for (var i = 0; i < songArray.length; i++) {
@@ -31,3 +65,26 @@ for (var i = 0; i < songArray.length; i++) {
 		songArray[i].classList.add("boldtext");
 	};
 }
+
+
+}
+
+// Create an XHR object
+var myRequest = new XMLHttpRequest();
+
+// XHR objects emit events when their operation is complete, or an error occurs
+myRequest.addEventListener("load", songsLoaded);
+// myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+
+// Then tell the XHR object exactly what to do
+myRequest.open("GET", "songs.json");
+
+// Tell the XHR object to start
+myRequest.send();
+
+
+
+
+
+
+
